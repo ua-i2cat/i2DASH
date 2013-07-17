dc3a3b2c663e47a9f9afb9c1d63974a4a636a302
El Taha, Nadim <nadim.el.taha@i2cat.net>
2013-07-17 10:44:02 +0200

    add fragment defaults struct

--- include/fragment.h
+++ include/fragment.h
@@ -5,8 +5,17 @@
 #include "context.h"
 #include "sample.h"
 
+typedef struct {
+	u32 track_number;
+	u32 duration;
+	u32 size;
+	u32 description_index;
+	u32 random_access;
+    u16 default_degradation_priority;
+    u8 default_padding;
+} i2DASHFragmentDefaults;
 
-i2DASHError i2dash_fragment_open();
+i2DASHError i2dash_fragment_open(i2DASHContext *context, GF_ISOFile *file);
 
 i2DASHError i2dash_fragment_add_sample();
 
