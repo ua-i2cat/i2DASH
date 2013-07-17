43c1bee1cb55fb0e1b2e9fe86f550299d64bbf37
El Taha, Nadim <nadim.el.taha@i2cat.net>
2013-07-17 10:02:01 +0200

    Add fragment.h basic headers

--- include/fragment.h
+++ include/fragment.h
@@ -1,5 +1,16 @@
 #ifndef _FRAGMENT_H_
 #define _FRAGMENT_H_
 
+#include <libavcodec/avcodec.h>
+#include "context.h"
+#include "sample.h"
+
+
+i2DASHError i2dash_fragment_open();
+
+i2DASHError i2dash_fragment_add_sample();
+
+i2DASHError i2dash_fragment_close();
 
 #endif
+
