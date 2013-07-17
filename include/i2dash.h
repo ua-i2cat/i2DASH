f85dc76b048b096041411fea878729ffc133b720
Contreras, Ignacio <ignacio.contreras@i2cat.net>
2013-07-17 10:26:00 +0000

    Add i2dash_write_frame function

--- include/i2dash.h
+++ include/i2dash.h
@@ -4,4 +4,18 @@
 #include "context.h"
 #include "sample.h"
 #include "error.h"
+
+
+/**
+ * @brief writes a new frame to the context's ISOM file
+ *
+ * @param context context that identifies the segmentation in process
+ * @param buffer buffer containing the NALs forming the frame
+ * @param buffer_len length in bytes of buffer
+ *
+ * @return i2DASH_OK if no error was found.
+ * @return i2DASH_ERROR otherwise.
+ */
+i2DASHError i2dash_write_frame(i2DASHContext *context, const char *buffer, int buffer_len);
+
 #endif
--- src/i2dash.c
+++ src/i2dash.c
@@ -1,1 +1,24 @@
 #include "i2dash.h"
+
+i2DASHError i2dash_write_frame(i2DASHContext *context, const char *buffer, int buffer_len)
+{   
+    int next_frame_number = context->frame_number + 1;
+
+    if (context->frame_number == 0) {
+        if (i2dash_segment_start(context) != i2DASH_OK) {
+            return i2DASH_ERROR;
+        }
+    }
+    else if (next_frame_number % context->frames_per_segment == 0) {
+        if (i2dash_segment_close(context) != i2DASH_OK) {
+            return i2DASH_ERROR;
+        };
+        if (i2dash_segment_start(context) != i2DASH_OK) {
+            return i2DASH_ERROR;
+        }
+    }
+
+    i2DASHError ret = i2dash_segment_write_frame(context, buffer, buffer_len);
+    
+    return ret;
+}
