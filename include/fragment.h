e16f23c9be4233a84af49fce4f6aa6ee13acb170
El Taha, Nadim <nadim.el.taha@i2cat.net>
2013-07-22 12:27:21 +0200

    rename i2dash_fragment_opem

--- include/fragment.h
+++ include/fragment.h
@@ -6,7 +6,7 @@
 #include "sample.h"
 
 
-i2DASHError i2dash_fragment_open(i2DASHContext *context);
+i2DASHError i2dash_fragment_setup(i2DASHContext *context);
 
 i2DASHError i2dash_fragment_write(i2DASHContext *context, const char * buf,
                           int buf_len, int dts, int key_frame);
