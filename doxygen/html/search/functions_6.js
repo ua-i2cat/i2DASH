fad033ad2d762ec990ac46973c8ad55856c38f67
Xavier Carol <xavier_carol@i2cat.net>
2015-01-09 13:18:38 +0100

    Added doxyfiles Remove obsolete README

---
+++ doxygen/html/search/functions_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['init',['init',['../classDashAudioSegmenter.html#a8b0d73777d048003e0df8c2fcfa22640',1,'DashAudioSegmenter::init()'],['../classDashVideoSegmenter.html#a27cb8ff8d87a4e5a464ea734d8b074e2',1,'DashVideoSegmenter::init()']]],
+  ['isinputopen',['isInputOpen',['../classDemuxer.html#ab8a20143ab739bcf8701f9d1cf7bf8a4',1,'Demuxer']]],
+  ['isintra',['isIntra',['../classAVCCFrame.html#ac33e084662b5dfbf4b1603eb7784eb45',1,'AVCCFrame']]]
+];
