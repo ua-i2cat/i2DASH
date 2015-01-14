5155d2526e60d93c9b407eaca44eb3ceffc59e23
marc.palau <marc.palau@i2cat.net>
2015-01-14 16:26:20 +0100

    Added new classes to generate MPD file

--- segmenter/include/MpdManager.hh
+++ segmenter/include/MpdManager.hh
@@ -21,6 +21,17 @@
 #ifndef _MPD_MANAGER_HH_
 #define _MPD_MANAGER_HH_
 
+#include <map>
+#include <deque>
+#include "tinyxml2.h"
+
+class Mpd;
+class AdaptationSet;
+class VideoAdaptationSet;
+class AudioAdaptationSet;
+class VideoRepresentation;
+class AudioRepresentation;
+
 class MpdManager
 {
 public:
@@ -29,6 +40,121 @@ public:
 
     bool writeSkeleton(const char* fileName);
     bool updateMpd(const char* fileName);
+
+private:
+    Mpd* mpd;
+};
+
+class Mpd
+{
+public:
+    Mpd();
+    virtual ~Mpd();
+
+    void writeToDisk(const char* fileName);
+
+private:
+    std::string minimumUpdatePeriod;
+    std::string timeShiftBufferDepth;
+    std::string suggestedPresentationDelay;
+    std::string minBufferTime;
+    std::string location;
+
+    std::map<std::string, AdaptationSet*> adaptationSets;
+
+};
+
+class AdaptationSet
+{
+public:
+    AdaptationSet();
+    virtual ~AdaptationSet();
+    virtual void toMpd(tinyxml2::XMLDocument& doc, tinyxml2::XMLElement*& adaptSet);
+
+protected:
+    bool segmentAlignment;
+    int startWithSAP;
+    bool subsegmentAlignment;
+    int subsegmentStartsWithSAP;
+    std::string mimeType;
+    int timescale;
+    std::string segTemplate;
+    std::string initTemplate;
+    int segmentDuration;
+    std::deque<int> timestamps;
+
+};
+
+class VideoAdaptationSet : public AdaptationSet 
+{
+public:
+    VideoAdaptationSet();
+    virtual ~VideoAdaptationSet();
+    void toMpd(tinyxml2::XMLDocument& doc, tinyxml2::XMLElement*& adaptSet);
+
+private:
+    std::map<std::string, VideoRepresentation*> representations;
+    
+    int maxWidth;
+    int maxHeight;
+    std::string par;
+    int frameRate;
+};
+
+class AudioAdaptationSet : public AdaptationSet 
+{
+public:
+    AudioAdaptationSet();
+    virtual ~AudioAdaptationSet();
+    void toMpd(tinyxml2::XMLDocument& doc, tinyxml2::XMLElement*& adaptSet);
+
+private:
+    std::map<std::string, AudioRepresentation*> representations;
+
+    std::string lang;
+    int audioSamplingRate;
+    std::string roleSchemeIdUri;
+    int roleValue;
+};
+
+class VideoRepresentation
+{
+public:
+    VideoRepresentation();
+    virtual ~VideoRepresentation();
+
+    std::string getCodec() {return codec;};
+    std::string getSAR() {return sar;};
+    int getWidth() {return width;};
+    int getHeight() {return height;};
+    int getBandwidth() {return bandwidth;};
+
+private:
+    std::string codec;
+    std::string sar;
+    int width;
+    int height;
+    int bandwidth;
+};
+
+class AudioRepresentation
+{
+public:
+    AudioRepresentation();
+    virtual ~AudioRepresentation();
+
+    std::string getCodec() {return codec;};
+    int getSampleRate() {return sampleRate;};
+    std::string getAudioChannelConfigSchemeIdUri() {return audioChannelConfigSchemeIdUri;};
+    int getBandwidth() {return bandwidth;};
+    int getAudioChannelConfigValue() {return audioChannelConfigValue;};
+
+private:
+    std::string codec;
+    int sampleRate;
+    int bandwidth;
+    std::string audioChannelConfigSchemeIdUri;
+    int audioChannelConfigValue;
 };
 
 #endif /* _MPD_MANAGER_HH_ */
--- segmenter/src/MpdManager.cpp
+++ segmenter/src/MpdManager.cpp
@@ -57,3 +57,149 @@ bool MpdManager::updateMpd(const char* fileName)
 
     return true;
 }
+
+void Mpd::writeToDisk(const char* fileName)
+{
+    tinyxml2::XMLDocument doc;
+    tinyxml2::XMLElement* root;
+    tinyxml2::XMLElement* period;
+    tinyxml2::XMLElement* el;
+
+    root = doc.NewElement("MPD");
+    root->SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+    root->SetAttribute("xmlns", "urn:mpeg:dash:schema:mpd:2011");
+    root->SetAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
+    root->SetAttribute("xsi:schemaLocation", "urn:mpeg:DASH:schema:MPD:2011 http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-DASH_schema_files/DASH-MPD.xsd");
+    root->SetAttribute("profiles", "urn:mpeg:dash:profile:isoff-live:2011");
+    root->SetAttribute("type", "dynamic");
+    root->SetAttribute("minimumUpdatePeriod", minimumUpdatePeriod.c_str());
+    root->SetAttribute("availabilityStartTime", "2014-10-29T03:07:39");
+    root->SetAttribute("timeShiftBufferDepth", timeShiftBufferDepth.c_str());
+    root->SetAttribute("suggestedPresentationDelay", suggestedPresentationDelay.c_str());
+    root->SetAttribute("minBufferTime", minBufferTime.c_str());
+    doc.InsertFirstChild(root);
+
+    el = doc.NewElement("ProgramInformation");
+    el->SetText("Demo DASH");
+    root->InsertEndChild(el);
+
+    el = doc.NewElement("Location");
+    el->SetText(location.c_str());
+    root->InsertEndChild(el);
+
+    period = doc.NewElement("Period");
+    period->SetAttribute("id", 0);
+    period->SetAttribute("start", "PT0.0S");
+
+    for (auto ad : adaptationSets) {
+        el = doc.NewElement("AdaptationSet");
+        el->SetAttribute("id", ad.first.c_str());
+        ad.second->toMpd(doc, el);
+        period->InsertEndChild(el);
+    }
+
+    root->InsertFirstChild(period);
+    doc.SaveFile("SavedData.xml");
+}
+
+void VideoAdaptationSet::toMpd(tinyxml2::XMLDocument& doc, tinyxml2::XMLElement*& adaptSet)
+{
+    tinyxml2::XMLElement* segmentTemplate;
+    tinyxml2::XMLElement* segmentTimeline;
+    tinyxml2::XMLElement* s;
+    tinyxml2::XMLElement* repr;
+
+    adaptSet->SetAttribute("mimeType", mimeType.c_str());
+    adaptSet->SetAttribute("maxWidth", maxWidth);
+    adaptSet->SetAttribute("maxHeight", maxHeight);
+    adaptSet->SetAttribute("par", par.c_str());
+    adaptSet->SetAttribute("frameRate", frameRate);
+    adaptSet->SetAttribute("segmentAlignment", segmentAlignment);
+    adaptSet->SetAttribute("startWithSAP", startWithSAP);
+    adaptSet->SetAttribute("subsegmentAlignment", subsegmentAlignment);
+    adaptSet->SetAttribute("subsegmentStartsWithSAP", subsegmentStartsWithSAP);
+
+    segmentTemplate = doc.NewElement("SegmentTemplate");
+    segmentTemplate->SetAttribute("timescale", timescale);
+    segmentTemplate->SetAttribute("media", segTemplate.c_str());
+    segmentTemplate->SetAttribute("initialization", initTemplate.c_str());
+
+    segmentTimeline = doc.NewElement("SegmentTimeline");
+
+    for (auto ts : timestamps) {
+        s = doc.NewElement("S");
+        s->SetAttribute("t", ts);
+        s->SetAttribute("d", segmentDuration);
+        segmentTimeline->InsertEndChild(s);
+    }
+
+    segmentTemplate->InsertEndChild(segmentTimeline);
+
+    for (auto r : representations) {
+        repr = doc.NewElement("Representation");
+        repr->SetAttribute("id", r.first.c_str());
+        repr->SetAttribute("codecs", r.second->getCodec().c_str());
+        repr->SetAttribute("width", r.second->getWidth());
+        repr->SetAttribute("height", r.second->getHeight());
+        repr->SetAttribute("sar", r.second->getSAR().c_str());
+        repr->SetAttribute("bandwidth", r.second->getBandwidth());
+        segmentTemplate->InsertEndChild(repr);
+    }
+
+    adaptSet->InsertEndChild(segmentTemplate);
+}
+
+void AudioAdaptationSet::toMpd(tinyxml2::XMLDocument& doc, tinyxml2::XMLElement*& adaptSet)
+{
+    tinyxml2::XMLElement* segmentTemplate;
+    tinyxml2::XMLElement* segmentTimeline;
+    tinyxml2::XMLElement* role;
+    tinyxml2::XMLElement* audioChannelConfiguration;
+    tinyxml2::XMLElement* s;
+    tinyxml2::XMLElement* repr;
+
+    adaptSet->SetAttribute("mimeType", mimeType.c_str());
+    adaptSet->SetAttribute("lang", lang.c_str());
+    adaptSet->SetAttribute("segmentAlignment", segmentAlignment);
+    adaptSet->SetAttribute("startWithSAP", startWithSAP);
+    adaptSet->SetAttribute("subsegmentAlignment", subsegmentAlignment);
+    adaptSet->SetAttribute("subsegmentStartsWithSAP", subsegmentStartsWithSAP);
+
+    role = doc.NewElement("Role");
+    role->SetAttribute("schemeIdUri", roleSchemeIdUri.c_str());
+    role->SetAttribute("value", roleValue);
+    adaptSet->InsertEndChild(role);
+
+    segmentTemplate = doc.NewElement("SegmentTemplate");
+    segmentTemplate->SetAttribute("timescale", timescale);
+    segmentTemplate->SetAttribute("media", segTemplate.c_str());
+    segmentTemplate->SetAttribute("initialization", initTemplate.c_str());
+
+    segmentTimeline = doc.NewElement("SegmentTimeline");
+
+    for (auto ts : timestamps) {
+        s = doc.NewElement("S");
+        s->SetAttribute("t", ts);
+        s->SetAttribute("d", segmentDuration);
+        segmentTimeline->InsertEndChild(s);
+    }
+
+    segmentTemplate->InsertEndChild(segmentTimeline);
+
+    for (auto r : representations) {
+        repr = doc.NewElement("Representation");
+        repr->SetAttribute("id", r.first.c_str());
+        repr->SetAttribute("codecs", r.second->getCodec().c_str());
+        repr->SetAttribute("audioSamplingRate", r.second->getSampleRate());
+        repr->SetAttribute("bandwidth", r.second->getBandwidth());
+
+        audioChannelConfiguration = doc.NewElement("AudioChannelConfiguration");
+        audioChannelConfiguration->SetAttribute("schemeIdUri", r.second->getAudioChannelConfigSchemeIdUri().c_str());
+        audioChannelConfiguration->SetAttribute("value", r.second->getAudioChannelConfigValue());
+        repr->InsertEndChild(audioChannelConfiguration);
+
+        segmentTemplate->InsertEndChild(repr);
+    }
+
+    adaptSet->InsertEndChild(segmentTemplate);
+}
